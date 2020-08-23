6e422fe0ffdfd9b7e294f2e0f228b3e3f8b053ab
Patrick Dunn <pmdunn78@gmail.com>
2020-08-23 18:55:52 -0400

    react router to basic login page

---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.css";
+import { Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
+
+
+const Login = () => {
+  return (
+
+    <div className="container">
+      <div className="child blue">Welcome to the Login Screen!</div>
+    </div>
+  );
+};
+
+export default Login;
