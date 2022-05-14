1a2353359121e602ab4894a3bfa09dba3939522d
Patrick Dunn <pmdunn78@gmail.com>
2022-05-14 17:21:49 -0400

    exporting all models for use

---
+++ server/models/index.js
@@ -0,0 +1,25 @@
+const Account = require("./Account");
+const Category = require("./Category");
+const Dollars = require("./Dollars");
+const Expense = require("./Expense");
+const Income = require("./Income");
+const Month = require("./Month");
+const Pesos = require("./Pesos");
+const ProjectedIncome = require("./ProjectedIncome");
+const SubCategory = require("./SubCategory");
+const User = require("./User");
+const Year = require("./Year");
+
+module.exports = {
+    Account,
+    Category,
+    Dollars,
+    Expense,
+    Income,
+    Month,
+    Pesos,
+    ProjectedIncome,
+    SubCategory,
+    User,
+    Year
+};
