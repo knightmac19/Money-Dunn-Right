1e94e0fb3156663351e5f86120e168866a3726c4
Patrick Dunn <pmdunn78@gmail.com>
2023-03-11 09:32:54 -0500

    GET, GET/:id, & POST controllers defined

---
+++ server/controllers/transactionController.js
@@ -0,0 +1,54 @@
+const Transaction = require ('../models/TransactionModel');
+const mongoose = require('mongoose');
+
+
+// get all transactions
+const getTransactions = async (req, res) => {
+  const transactions = await Transaction.find({}).sort({date: 1});
+
+  res.status(200).json(transactions);
+}
+
+
+// get a single transaction
+const getSingleTransaction = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such transaction'});
+  }
+
+  const transaction = await Transaction.findById(id);
+
+  if (!transaction) {
+    return res.status(404).json({error: 'No such transaction'});
+  }
+
+  res.status(200).json(transaction);
+}
+
+
+// create a new transaction
+const createTransaction = async (req, res) => {
+  const {date, amount, category, description, account} = req.body;
+
+  try {
+    const transaction = await Transaction.create({date, amount, category, description, account});
+    res.status(200).json(transaction);
+  } catch (err) {
+    res.status(400).json({
+      error: err.message
+    })
+  }
+}
+
+// delete a transaction
+
+
+// update a transaction
+
+module.exports = {
+  createTransaction,
+  getTransactions,
+  getSingleTransaction
+}
--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,36 +1,21 @@
 const express = require('express');
 const Transaction = require('../models/TransactionModel');
+const {
+  createTransaction,
+  getTransactions,
+  getSingleTransaction
+} = require('../controllers/transactionController');
 
 const router = express.Router();
 
 // GET all transactions
-router.get('/', (req, res) => {
-  res.json({
-    mssg: 'GET all transactions'
-  })
-});
+router.get('/', getTransactions);
 
 // GET a single transaction
-router.get('/:id', (req, res) => {
-  res.json({
-    mssg: 'GET a single transaction'
-  })
-});
+router.get('/:id', getSingleTransaction);
 
 // POST a new transaction
-router.post('/', async (req, res) => {
-  const {date, amount, category, description, account} = req.body;
-
-  try {
-    const transaction = await Transaction.create({date, amount, category, description, account});
-    res.status(200).json(transaction);
-  } catch (err) {
-    res.status(400).json({
-      error: err.message
-    })
-  }
-
-});
+router.post('/', createTransaction);
 
 // DELETE a single transaction
 router.delete('/:id', (req, res) => {
