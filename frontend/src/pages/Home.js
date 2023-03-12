e81047b4aa055003bb66a2e3228ecc96c3d78379
Patrick Dunn <pmdunn78@gmail.com>
2023-03-12 10:55:33 -0400

    fetching data from backend, mapping over and rendering <p> for each description

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
+
 const Home = () => {
+
+  const [transactions, setTransactions] = useState(null);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      const res = await fetch('/api/transactions');
+      const json = await res.json();
+
+      if (res.ok){
+        setTransactions(json);
+      }
+    }
+
+    fetchTransactions();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="transactions">
+        { transactions && transactions.map((transaction) => (
+          <p key={transaction._id}>{transaction.description}</p>
+        ))}
+      </div>
     </div>
   )
 }
