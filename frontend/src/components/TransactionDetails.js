f7af71f3fa136e6c40293d650b91bc17e2985522
Patrick Dunn <pmdunn78@gmail.com>
2023-03-12 12:03:17 -0400

    update order of info rendered for each transaction

--- frontend/src/components/TransactionDetails.js
+++ frontend/src/components/TransactionDetails.js
@@ -2,8 +2,8 @@ const TransactionDetails = ({ transaction }) => {
 
   return (
     <div className="transaction-details">
-      <h4>{transaction.category}</h4>
-      <p><strong>Amount: </strong>{transaction.amount}</p>
+      <h4>$ {transaction.amount}</h4>
+      <p><strong>Category: </strong>{transaction.category}</p>
       <p><strong>Note: </strong>{transaction.description}</p>
       <p><strong>Account: </strong>{transaction.account}</p>
       <p>{transaction.date}</p>
