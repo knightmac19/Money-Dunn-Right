c5120c8b5074771b228ba271ce41113daf2d642d
Patrick Dunn <pmdunn78@gmail.com>
2023-03-15 21:16:00 -0400

    adding income model

---
+++ server/models/IncomeModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const incomeSchema = new Schema({
+  date: {
+    type: Date,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  source: {
+    type: String,
+    required: true
+  },
+
+}, { timestamps: true });
+
+const Income = mongoose.model('Income', incomeSchema);
+
+module.exports = Income;
+
