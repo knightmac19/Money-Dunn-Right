f3371df5d2aa0176411764085778e194c5fed9ec
Patrick Dunn <pmdunn78@gmail.com>
2023-03-15 23:51:34 -0400

    dynamically rendering expense / income form / hide based on state

--- frontend/src/components/TransactionForm.js
+++ frontend/src/components/TransactionForm.js
@@ -1,13 +1,19 @@
+import { useState } from 'react';
+import ExpenseForm from './ExpenseForm';
+
 // import ExpenseForm from './ExpenseForm';
 import IncomeForm from './IncomeForm';
 
 const TransactionForm = () => {
-  
+  const [form, setForm] = useState('EXPENSE')
+
+
 
   return (
     <div>
       {/* <ExpenseForm /> */}
-      <IncomeForm />
+      {/* <IncomeForm /> */}
+      {form === 'HIDE' ? <div></div> : (form === 'EXPENSE' ? <ExpenseForm /> : <IncomeForm />)}
     </div>
   )
 }
