fb00a474795a6488c9a84a12645bf4373afd6709
Patrick Dunn <pmdunn78@gmail.com>
2023-03-16 20:49:03 -0400

    setup user signup logic -- UserModel static create w/salt & hash

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,6 +9,6 @@ const router = express.Router();
 router.post('/login', loginUser);
 
 // signup route
-router.post('signup', signupUser);
+router.post('/signup', signupUser);
 
 module.exports = router;
