0a62a6aca044271031aeae8e7af3050bf536fe1b
Patrick Dunn <pmdunn78@gmail.com>
2023-03-17 19:02:00 -0400

    setup authcontext & provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { TransactionsContextProvider } from './context/TransactionContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TransactionsContextProvider>
-      <App />
-    </TransactionsContextProvider>
+    <AuthContextProvider>
+      <TransactionsContextProvider>
+        <App />
+      </TransactionsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
