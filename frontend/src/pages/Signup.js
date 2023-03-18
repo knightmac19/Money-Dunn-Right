fab0ab88c22ca41c4af574e9e0cb6eacc62c6151
Patrick Dunn <pmdunn78@gmail.com>
2023-03-18 00:29:27 -0400

    functional signup with input validation and jwt local storage

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const {signup, error, isLoading} = useSignup()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    
+    await signup(email, password)
   }
 
   return (
@@ -29,10 +32,11 @@ const Signup = () => {
           value={password}
         />
 
-        <button className='add-transaction-btn'>Sign up</button>
+        <button disabled={isLoading} className='add-transaction-btn'>Sign up</button>
         <div className='form-link'>
           <Link to="/login">Or log in instead</Link>
         </div>
+        {error && <div className='error'>{error}</div>}
       </form>
     </div>
     
