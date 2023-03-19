15b5940474928b77ee2447be70843fb95a2c5419
Patrick Dunn <pmdunn78@gmail.com>
2023-03-18 23:15:17 -0400

    add useLogout hook, useLogout btn + styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+
+  const handleClick = (e) => {
+    logout()
+  }
 
   return (
     <header>
@@ -10,6 +16,9 @@ const Navbar = () => {
           <h1 className="nav-header-small">MDR</h1>
         </Link>
         <nav>
+          <div>
+            <button onClick={handleClick}>Log out</button>
+          </div>
           <div className="nav-container">
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
