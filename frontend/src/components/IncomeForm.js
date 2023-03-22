69bf21c5718d86a6f4e11b46c728ee28bc8a2832
Patrick Dunn <pmdunn78@gmail.com>
2023-03-22 12:59:35 -0400

    adding trailing slashes to try to overcome CORS

--- frontend/src/components/IncomeForm.js
+++ frontend/src/components/IncomeForm.js
@@ -36,7 +36,7 @@ const IncomeForm = () => {
 
     const income = { amount, source, date }
 
-    const response = await fetch('https://money-dunn-right.onrender.com/api/income', {
+    const response = await fetch('https://money-dunn-right.onrender.com/api/income/', {
       method: 'POST',
       body: JSON.stringify(income),
       headers: {
