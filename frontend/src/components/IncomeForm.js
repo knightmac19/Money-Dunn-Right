f47eb0a3d9e9dd64b2d66f71271827fa7cf33c62
Patrick Dunn <pmdunn78@gmail.com>
2023-04-21 19:18:48 -0400

    TransactionLoader for Expense & Income forms

--- frontend/src/components/IncomeForm.js
+++ frontend/src/components/IncomeForm.js
@@ -4,12 +4,14 @@ import { useTransactionsContext } from "../hooks/useTransactionsContext";
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useLangContext } from '../hooks/useLangContext';
 import { Spanish, English } from './LangText/IncomeFormText';
+import TransactionLoader from './TransactionLoader';
 
 const IncomeForm = () => {
   const { dispatch } = useTransactionsContext();
   const { user } = useAuthContext();
   const { language } = useLangContext();
   
+  const [loading, setLoading] = useState(false)
   const [lang, setLang] = useState(English);
   const [amount, setAmount] = useState('');
   const [source, setSource] = useState('');
@@ -41,6 +43,7 @@ const IncomeForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true)
 
     if (!user) {
       setError(lang.notUserErr);
@@ -73,54 +76,59 @@ const IncomeForm = () => {
       setEmptyFields([]);
       console.log('New Income Added!');
       dispatch({type: 'CREATE_TRANSACTION', payload: json})
+      setLoading(false)
     }
   }
 
   return (
-    <form className='create' onSubmit={handleSubmit}>
-      
+    <div>
+      { loading ? <TransactionLoader></TransactionLoader> : 
+        <form className='create' onSubmit={handleSubmit}>
+        
 
-      <label>{lang.amountLabel}</label>
-      <input
-        type="number"
-        step="0.01"
-        placeholder="0.00"
-        min="0.00"
-        onChange={(e) => setAmount(e.target.value)}
-        value={amount}
-        className={emptyFields.includes('amount') ? 'error' : ''}
-      />
+        <label>{lang.amountLabel}</label>
+        <input
+          type="number"
+          step="0.01"
+          placeholder="0.00"
+          min="0.00"
+          onChange={(e) => setAmount(e.target.value)}
+          value={amount}
+          className={emptyFields.includes('amount') ? 'error' : ''}
+        />
 
-      <label>{lang.sourceLabel}</label>
-      <input
-        type="text"
-        maxLength="30"
-        minLength="0"
-        placeholder={lang.sourcePlaceholder}
-        onChange={(e) => setSource(e.target.value)}
-        value={source}
-        className={emptyFields.includes('source') ? 'error' : ''}
-      />
+        <label>{lang.sourceLabel}</label>
+        <input
+          type="text"
+          maxLength="30"
+          minLength="0"
+          placeholder={lang.sourcePlaceholder}
+          onChange={(e) => setSource(e.target.value)}
+          value={source}
+          className={emptyFields.includes('source') ? 'error' : ''}
+        />
 
-      <label>{lang.dateLabel}</label>
-      <input
-        type="date"
-        placeholder={lang.datePlaceholder}
-        onChange={(e) => validateDate(e.target.value)}
-        value={date}
-        className={emptyFields.includes('date') ? 'error' : ''}
-      />
-      <br />
-      <span 
-        style={{
-          fontWeight: 'bold',
-          color: 'red',
-        }}
-      >{dateError}</span>
+        <label>{lang.dateLabel}</label>
+        <input
+          type="date"
+          placeholder={lang.datePlaceholder}
+          onChange={(e) => validateDate(e.target.value)}
+          value={date}
+          className={emptyFields.includes('date') ? 'error' : ''}
+        />
+        <br />
+        <span 
+          style={{
+            fontWeight: 'bold',
+            color: 'red',
+          }}
+        >{dateError}</span>
 
-      <button className='add-transaction-btn'>{lang.buttonText}</button>
-      {error && <div className='error'>{error}</div>}
-    </form>
+        <button className='add-transaction-btn'>{lang.buttonText}</button>
+        {error && <div className='error'>{error}</div>}
+      </form>
+    }
+  </div>
   )
 }
 
