f0d16b10731e466947fe2ac15f7770135c6dd5fe
Patrick Dunn <pmdunn78@gmail.com>
2023-04-21 18:31:40 -0400

    removing Link components for Login and Signup

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
 import { useLogin } from '../hooks/useLogin'
 import { useLangContext } from '../hooks/useLangContext'
 import { Spanish, English } from '../components/LangText/LoginPageText'
