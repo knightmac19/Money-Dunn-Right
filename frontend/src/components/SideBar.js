4992ea5f640800d783c5f16e880545028f127029
Patrick Dunn <pmdunn78@gmail.com>
2023-04-26 19:12:07 -0400

    initializing peer reqs & peers list in sidebar

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -3,15 +3,24 @@ import { useLangContext } from '../hooks/useLangContext';
 
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
 import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
+import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import { peerRequests } from './peerRequestsData';
+import { peers } from './peersData'
 
 
 const SideBar = () => {
+
+  
 
   const { dispatch } = useLangContext();
 
   const [englishFocused, setEnglishFocused] = useState('english-btn-focused')
   const [spanishFocused, setSpanishFocused] = useState('')
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  // const [acceptClicks, setAcceptClicks] = useState(0)
+  const [acceptIconSize, setAcceptIconSize] = useState('medium')
+  // const [acceptIconClass, setAcceptIconClass] = useState('accept-request-icon')
 
   const handleClick = (e) => {
     let btn = e.target.name
@@ -29,8 +38,22 @@ const SideBar = () => {
     }
   }
 
+  const acceptRequestClick = (e) => {
+    console.log('accept request')
+    console.log(e.target)
+    setAcceptIconSize('large')
+  }
+
+  const rejectRequestClick = (e) => {
+    console.log('reject request')
+  }
+
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
+  }
+
+  const removePeerClick = (e) => {
+    console.log('removePeerClick')
   }
 
   return (
@@ -56,7 +79,40 @@ const SideBar = () => {
         >
           Español
         </button>
+        
       </div>
+      <div className={sidebarOpen ? "" : "peers-container-hide"}>
+        <div className="peers">
+          <ul>My Peers
+            {peers && peers.map((peer) => (
+              <li key={peer.id} className="peers-item">{peer.email} 
+                <CloseOutlinedIcon 
+                  className="reject-request-icon"
+                  onClick={removePeerClick}
+                />
+              </li>  
+            ))}
+          </ul>
+        </div>
+        <div className="peer-requests">
+          <ul>Peer Requests
+            {peerRequests && peerRequests.map((peerRequest) => (
+              <li key={peerRequest.id} className="peer-request-item">{peerRequest.email} 
+                <CheckCircleOutlinedIcon  
+                  className='accept-request-icon'
+                  onClick={acceptRequestClick}
+                  fontSize={acceptIconSize}
+                />
+                <CloseOutlinedIcon 
+                  className="reject-request-icon"
+                  onClick={rejectRequestClick}
+                />  
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      
     </div>
   )
 }
---
+++ frontend/src/components/peerRequestsData.js
@@ -0,0 +1,11 @@
+export const peerRequests = [
+  {
+    id: 0,
+    email: 'princess@peach.com'
+  },
+  {
+    id: 1,
+    email: 'mario@kart.com'
+  },
+]
+
