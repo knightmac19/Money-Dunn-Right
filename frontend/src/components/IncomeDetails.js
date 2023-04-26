d48211af748b9b7e80daef3c1d67ab1f7e5f51b6
Patrick Dunn <pmdunn78@gmail.com>
2023-04-26 16:45:31 -0400

    setting api urls back for main

--- frontend/src/components/IncomeDetails.js
+++ frontend/src/components/IncomeDetails.js
@@ -72,7 +72,7 @@ const IncomeDetails = ({ transaction }) => {
 
     if (trashClicks > 0) {
       setShowDualRing(true)
-      const res = await fetch('/api/income/' + transaction._id, {
+      const res = await fetch('https://money-dunn-right.onrender.com/api/income/' + transaction._id, {
         method: 'DELETE',
         headers: {
           'Authorization': `Bearer ${user.token}`
