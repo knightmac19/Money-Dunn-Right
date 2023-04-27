d35ef99920e7ce567e45dc498ff9674de58949b6
Patrick Dunn <pmdunn78@gmail.com>
2023-04-26 20:48:02 -0400

    toggle sidebar language text

---
+++ frontend/src/components/LangText/SideBarText.js
@@ -0,0 +1,9 @@
+export const Spanish = {
+  peersListText: 'Mis Amigos',
+  peerRequestsText: 'Solicitudes'
+}
+
+export const English = {
+  peersListText: 'Friends',
+  peerRequestsText: 'Requests'
+}
