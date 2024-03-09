57750bc733adb72c90f51316466720d7cb15c004
Patrick Dunn <pmdunn78@gmail.com>
2024-03-09 15:53:57 -0500

    removing sidebar from production

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,54 +1,52 @@
 import { useEffect } from "react";
 import { useTransactionsContext } from "../hooks/useTransactionsContext";
-import { useAuthContext } from '../hooks/useAuthContext'
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // components
 import TransactionDetails from "../components/TransactionDetails";
 import IncomeDetails from "../components/IncomeDetails";
 import TransactionForm from "../components/TransactionForm";
-import SideBar from "../components/SideBar";
-
-
 
 const Home = () => {
-  const {transactions, dispatch} = useTransactionsContext();
-  const {user} = useAuthContext()
-
-  
+  const { transactions, dispatch } = useTransactionsContext();
+  const { user } = useAuthContext();
 
   // sort by date
   const byDate = (a, b) => {
     return new Date(b.date).valueOf() - new Date(a.date).valueOf();
-  }
-  
+  };
 
   useEffect(() => {
-    
     const fetchTransactions = async () => {
       // console.log('is fetchTransactions firing?')
-      const expensesResponse = await fetch('https://money-dunn-right.onrender.com/api/expenses/', {
-        headers: {
-          'Authorization': `Bearer ${user.token}`
+      const expensesResponse = await fetch(
+        "https://money-dunn-right.onrender.com/api/expenses/",
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
         }
-      });
+      );
       const expensesJson = await expensesResponse.json();
 
-      const incomeResponse = await fetch('https://money-dunn-right.onrender.com/api/income/', {
-        headers: {
-          'Authorization': `Bearer ${user.token}`
+      const incomeResponse = await fetch(
+        "https://money-dunn-right.onrender.com/api/income/",
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
         }
-      });
+      );
       const incomeJson = await incomeResponse.json();
-      
+
       const fullJson = await expensesJson.concat(incomeJson);
 
-      const sortedJson = await fullJson.sort(byDate)
+      const sortedJson = await fullJson.sort(byDate);
 
-      if (expensesResponse.ok && incomeResponse.ok){
-        dispatch({type: 'SET_TRANSACTIONS', payload: sortedJson});
+      if (expensesResponse.ok && incomeResponse.ok) {
+        dispatch({ type: "SET_TRANSACTIONS", payload: sortedJson });
       }
-    }
-
+    };
 
     if (user) {
       fetchTransactions();
@@ -57,22 +55,27 @@ const Home = () => {
 
   return (
     <div>
-      
-    <div className="home">
-    <SideBar />
-      <TransactionForm />
-      <div className="transactions">
-        { transactions && transactions.map((transaction) => (
-          transaction.source ? 
-          <IncomeDetails key={transaction._id} transaction={transaction} /> :
-          <TransactionDetails key={transaction._id} transaction={transaction} />
-        ))}
+      <div className="home">
+        <TransactionForm />
+        <div className="transactions">
+          {transactions &&
+            transactions.map((transaction) =>
+              transaction.source ? (
+                <IncomeDetails
+                  key={transaction._id}
+                  transaction={transaction}
+                />
+              ) : (
+                <TransactionDetails
+                  key={transaction._id}
+                  transaction={transaction}
+                />
+              )
+            )}
+        </div>
       </div>
-      
     </div>
-    
-    </div>
-  )
-}
+  );
+};
 
 export default Home;
